69119ba197e23953cab26e9cb4853b10bc2a8566
Daniel <angelicodaniel93@gmail.com>
2022-11-02 20:15:26 +0100

    last commit

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,40 +2,49 @@ const griglia = document.getElementById('griglia')
 let button = document.getElementById('button')
 let difficulty = document.getElementById('difficulty')
 button.addEventListener('click', play) 
-let bombs = []
-let numbers = []
+let score = 0;
 
 function bombe(){
-    for ( i = 0; i < 16; i++){
-    let bomb = Math.floor(Math.random() * parseInt(difficulty.value * difficulty.value)) + 1;
-    bombs.push(bomb)
+    let bombs = []
+    for ( let i = 0; i < 16; i++){
+        let bomb;
+        let controlloBombe = false
+        while(!controlloBombe){
+            bomb = Math.floor(Math.random() * difficulty.value * difficulty.value);
+            if (!bombs.includes(bomb)){
+                controlloBombe = true
+            }
+        }
+        bombs.push(bomb)
     }
+    console.log(bombs)
+    return bombs
 }
-
 
 
 
 function play() {
     griglia.innerHTML = ''
-    let bombElement = bombe();
-    console.log(bombs)
+    let bombs = bombe();
     for ( let i = 1; i <= difficulty.value * difficulty.value; i++) {
     const box = document.createElement('div')
     box.classList.add('square')
     box.style.width = "calc(100% / "+difficulty.value+")"
     box.innerHTML = `${i}`
     griglia.append(box)
-    numbers.push(i)
     box.addEventListener('click', function () {
-        if (numbers.includes(bombs[i])){
+        if (bombs.includes(i)){
             i++
             this.classList.toggle('error')
+            alert('hai perso. il tuo punteggio è '+score)
         }   else {
             this.classList.toggle('active')
+            score++
+            
         }
 
     }) 
-}   console.log(numbers)
+}   
 }
 
 
