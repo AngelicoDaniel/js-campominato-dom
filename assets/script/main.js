c1f7842561f0263372a3ea43b7e207872aff7c57
Daniel <angelicodaniel93@gmail.com>
2022-11-02 15:34:12 +0100

    ho ricominciato

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,58 +1,22 @@
 const griglia = document.getElementById('griglia')
 let button = document.getElementById('button')
 let difficulty = document.getElementById('difficulty')
-button.addEventListener('click', game)
+button.addEventListener('click', play) 
 
-let arrBombs = []
-
-function randomBombs (maxSquares){
-    for( let i = 0; i < 16; i++){
-        let randomBombs = Math.floor(Math.random() * maxSquares)
-        if (arrBombs.includes(randomBombs)){
-            i--;
-        }   else{
-            arrBombs.push(randomBombs)
-        }
-    }
-}
-
-
-function creazioneBox(){
-    div = document.createElement('div');
-    div.classList.add('square');
-    div.style.width = "calc(100%/"+difficulty.value+")";
-    return div;
-    //var aggettivo = "forte";
-    //var frase = "goku è " + aggettivo + "!";
-}
-console.log( creazioneBox() )
-
-
-function game(){
-
+function play() {
     griglia.innerHTML = ''
-    let maxSquares = difficulty.value*difficulty.value;
-    
-    for (let i = 1; i <= maxSquares; i++){
-        // let bombs = randomBombs();
-        let element = creazioneBox();
-        console.log(element)
-        element.innerHTML += `${i}`
-        element.addEventListener('click', function() {
-        console.log(this)
-        if ( this == arrBombs[i]){
-            this.classList.toggle('error')
-        }   else {
-            this.classList.toggle('active')
-        }
+    for ( let i = 1; i <= difficulty.value * difficulty.value; i++) {
+    const eleCell = document.createElement('div')
+    eleCell.classList.add('square')
+    eleCell.style.width = "calc(100% / "+difficulty.value+")"
+    eleCell.innerHTML = `${i}`
+    griglia.append(eleCell)
 
-
-    })
-
-    griglia.append (element)
-    }
+    eleCell.addEventListener('click', function () {
+        this.classList.toggle('active')  
+    }) 
 }
-
+}
 
 
 
