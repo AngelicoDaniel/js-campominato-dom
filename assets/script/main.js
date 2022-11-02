ddf8ce41a42570e2983027821d06b54878a263b0
Daniel <angelicodaniel93@gmail.com>
2022-11-02 15:52:52 +0100

    generato array bombe

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,6 +2,13 @@ const griglia = document.getElementById('griglia')
 let button = document.getElementById('button')
 let difficulty = document.getElementById('difficulty')
 button.addEventListener('click', play) 
+let bombs = []
+
+for ( i = 0; i < 16; i++){
+    let bomb = Math.floor(Math.random() * parseInt(difficulty.value * difficulty.value)) + 1;
+    bombs.push(bomb)
+}
+console.log(bombs)
 
 function play() {
     griglia.innerHTML = ''
@@ -11,6 +18,7 @@ function play() {
     eleCell.style.width = "calc(100% / "+difficulty.value+")"
     eleCell.innerHTML = `${i}`
     griglia.append(eleCell)
+    
 
     eleCell.addEventListener('click', function () {
         this.classList.toggle('active')  
