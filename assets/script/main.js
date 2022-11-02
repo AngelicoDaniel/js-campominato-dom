c4f04d350b2f38666a2f62f0861415b4f7b1f6b5
Daniel <angelicodaniel93@gmail.com>
2022-11-02 16:24:08 +0100

    funzione bombe portata nella main function

--- assets/script/main.js
+++ assets/script/main.js
@@ -3,27 +3,35 @@ let button = document.getElementById('button')
 let difficulty = document.getElementById('difficulty')
 button.addEventListener('click', play) 
 let bombs = []
+let numbers = []
 
-for ( i = 0; i < 16; i++){
+function bombe(){
+    for ( i = 0; i < 16; i++){
     let bomb = Math.floor(Math.random() * parseInt(difficulty.value * difficulty.value)) + 1;
     bombs.push(bomb)
 }
 console.log(bombs)
+}
+
+
+
 
 function play() {
     griglia.innerHTML = ''
+    let bombElement = bombe();
+    console.log(bombElement)
     for ( let i = 1; i <= difficulty.value * difficulty.value; i++) {
     const eleCell = document.createElement('div')
     eleCell.classList.add('square')
     eleCell.style.width = "calc(100% / "+difficulty.value+")"
     eleCell.innerHTML = `${i}`
     griglia.append(eleCell)
-    
+    numbers.push(i)
 
     eleCell.addEventListener('click', function () {
-        this.classList.toggle('active')  
+    this.classList.toggle('active')  
     }) 
-}
+}   console.log(numbers)
 }
 
 
